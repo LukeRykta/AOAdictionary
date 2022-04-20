20a72f1c6a6d4aaf20d46586362482409322f3ce
Brandon Stevenson <stevenson.brandon01@gmail.com>
2022-04-20 15:34:40 -0500

    created file parser

--- setHash.java
+++ setHash.java
@@ -1,6 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class setHash {
+    //Global Variables
     HashSet<String> input = new HashSet<String>();
+    //Constructor
     public setHash(){}
+
+    //File parser. Stores each line into Hashset.
+    public void parseFile(String file){
+        //Initialize File
+        File newFile = new File(file);
+        //Try catch for storing strings into hashSet
+        try{
+            Scanner scan = new Scanner(newFile);
+            String fileLine;
+            //While loop will iterate each line
+            while(scan.hasNextLine()){
+                fileLine = scan.nextLine();
+                input.add(fileLine);
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 }

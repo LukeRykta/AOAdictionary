9031c253de85bd477f45b958449644338f299754
Brandon Stevenson <stevenson.brandon01@gmail.com>
2022-04-29 13:45:10 -0500

    filled the c matrix bottom up style according to notes

--- dynamicAlgo.java
+++ dynamicAlgo.java
@@ -1,5 +1,4 @@
 import java.util.HashSet;
-import java.util.Iterator;
 
 public class dynamicAlgo {
     //Global Variables
@@ -10,19 +9,29 @@ public class dynamicAlgo {
     }
 
     public int max(int a, int b){
-        return 0;
+        return Math.max(a, b);
     }
 
-    public void splitAlgo(String word, String dictWord, int n){
+    public int splitAlgo(String word, String dictWord, int n){
         //Create iterator to iterate to each string in the Hashset
+        //I'll be basing the filling of the c matrix based off the exmpale given in the notes.
         int len = word.length();
         int[][] c = new int[len][n];
-        for(int i = 0; i < len; i++){
-            for(int j = 0; j < n; j++){
+        for(int i = 0; i < len; i++)
+            c[i][0] = 0;
+        for(int j = 0; j < n; j++)
+            c[0][j] = 0;
 
+        for(int i = 1; i < len; i++){
+            for(int j = 1; j < n; j++){
+                //start to fill in c
+                if(word.charAt(i) == dictWord.charAt(j))
+                    c[i][j] = c[i-1][j-1] + 1;
+                else
+                    c[i][j] = max(c[i-1][j], c[i][j-1]);
             }
         }
-
+        return c[len][n];
     }
 
     public HashSet<String> getSplitList() {

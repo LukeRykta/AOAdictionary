e272d379d5005c7645708c43b40ae050617a191a
Brandon Stevenson <stevenson.brandon01@gmail.com>
2022-04-29 17:30:16 -0500

    Fixed it lmao

--- Main.java
+++ Main.java
@@ -2,29 +2,18 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-        int total = 1; // number of splits
-        ArrayList<String> inputs = new ArrayList<String>(); // each index is a line of input.txt
-        int temp = 0;
+        String printString = "";
 
         setHash set = new setHash(); //Object for Hashset
         setDictionary dictionary = new setDictionary(); //Object for Hashset
         dictionary.parseFile("input/aliceInWonderlandDictionary.txt");
         set.parseFile("input/input.txt");
-
-        for (String s : set.input) {
-            //System.out.println(s);
-            inputs.add(s); //store each line of our input.txt to our arraylist to be referenced later
-        }
         dynamicAlgo answer = new dynamicAlgo();
 
-        for (String s : set.input) {
-            for (String d : dictionary.forReading) {
-                temp = answer.splitAlgo(s, d, d.length());
-                System.out.println("The max number of matches between " + s + " and " + d + " is: " + temp);
-            }
+        for(String s: set.input) {
+            printString = answer.parseString(s, dictionary.forReading);
+            System.out.println("The output is: " + printString);
         }
 
-        String[] matches = new String[total]; // literal string matches
-        printResults.printAll(total, inputs, matches);
     }
 }
--- dynamicAlgo.java
+++ dynamicAlgo.java
@@ -1,44 +1,36 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class dynamicAlgo {
-    //Global Variables
-    HashSet<String> splitList = new HashSet<String>();
+
     //Constructor
     dynamicAlgo(){
 
     }
-
-    public int max(int a, int b){
-        return Math.max(a, b);
+    public String parseString(String wholeWord, HashSet<String> dictionary){
+        return splitAlgo(wholeWord, new HashMap<String, String>(), dictionary);
     }
 
-    public int splitAlgo(String word, String dictWord, int n){
-        //Create iterator to iterate to each string in the Hashset
-        //I'll be basing the filling of the c matrix based off the exmpale given in the notes.
-        int len = word.length();
-        int[][] c = new int[len][n];
-        for(int i = 0; i < len; i++)
-            c[i][0] = 0;
-        for(int j = 0; j < n; j++)
-            c[0][j] = 0;
+    public String splitAlgo(String word, HashMap<String, String> newMap, HashSet<String> dictionary){
+        if(newMap.containsKey(word))
+            return newMap.get(word);
+        if(dictionary.contains(word))
+            return word;
 
-        for(int i = 1; i < len; i++){
-            for(int j = 1; j < n; j++){
-                //start to fill in c
-                if(word.charAt(i) == dictWord.charAt(j))
-                    c[i][j] = c[i-1][j-1] + 1;
-                else
-                    c[i][j] = max(c[i-1][j], c[i][j-1]);
+        for(int l = 1; l < word.length(); l++){
+            String lSub = word.substring(0, l);
+            if(!dictionary.contains(lSub))
+                continue;
+            String rSub = word.substring(l);
+            String rParsed = splitAlgo(rSub, newMap, dictionary);
+            if(rParsed != null){
+                String parsed = lSub + " " + rParsed;
+                newMap.put(word, parsed);
+                return parsed;
             }
         }
-        return c[len-1][n-1];
+        newMap.put(word, null);
+        return null;
     }
 
-    public HashSet<String> getSplitList() {
-        return splitList;
-    }
-
-    public void setSplitList(HashSet<String> splitList) {
-        this.splitList = splitList;
-    }
 }
